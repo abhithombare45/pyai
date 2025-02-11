1cdc5a574e2d3fbc75560f906a8add067855c931
Abhijeet Thombare <abhithombare45@gmail.com>
2025-02-11 08:40:01 +0530

    tryouts

--- python/learning_codes/test_add_txt_file_repeted_word_count.py
+++ python/learning_codes/test_add_txt_file_repeted_word_count.py
@@ -2,14 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-# def main():
-words = np.loadtxt(
-    "/Users/abhijeetthombare/vscode/python/learning_codes/address.txt",
-)
-print({words})
-lowercase_words = [word.lower() for word in words if len(word) >= 4]
-counts = {word: words.count(word) for word in lowercase_words}
-save.counts(counts)
+def main():
+    words = pd.read_txt(
+        "/Users/abhijeetthombare/vscode/python/learning_codes/address.txt"
+    )
+    lowercase_words = [word.lower() for word in words if len(word) >= 4]
+    counts = {word: words.count(word) for word in lowercase_words}
+    save.counts(counts)
 
 
 print("Abhi")
