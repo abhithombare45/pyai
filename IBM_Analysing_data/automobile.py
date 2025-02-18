63824516254bcd8080f7150627468f930d3203c9
Abhijeet Thombare <abhithombare45@gmail.com>
2025-02-18 15:57:35 +0530

    Practice...

--- IBM_Analysing_data/automobile.py
+++ IBM_Analysing_data/automobile.py
@@ -55,3 +55,43 @@ df_miss = df.replace("?", np.nan)
 
 df.dropna(subset=["price"], axis=0).head(30)
 # inplace=True
+df.replace("?", np.nan, inplace=True)
+
+missing_data = df.isnull()
+missing_data.head(5)
+
+# Count missing values in each column
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print(missing_data[column].value_counts())
+    print("")
+
+# "True" means the value is a missing value while "False" means the value is not a missing value.
+
+# Based on the summary above, each column has 205 rows of data and seven of the columns containing missing data:
+# <ol>
+#     <li>"normalized-losses": 41 missing data</li>
+#     <li>"num-of-doors": 2 missing data</li>
+#     <li>"bore": 4 missing data</li>
+#     <li>"stroke" : 4 missing data</li>
+#     <li>"horsepower": 2 missing data</li>
+#     <li>"peak-rpm": 2 missing data</li>
+#     <li>"price": 4 missing data</li>
+# </ol>
+
+avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
+print("Average of normalized-losses:", avg_norm_loss)
+
+df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
+
+avg_bore = df["bore"].astype("float").mean(axis=0)
+print("Average of bore:", avg_bore)
+
+df["bore"].replace(np.nan, avg_bore, inplace=True)
+
+df["stroke"].dtype
+# Write your code below and press Shift+Enter to execute
+mean_stroke = df["stroke"].astype("float").mean(axis=0)
+print("Avg of stroke:", mean_stroke)
+
+df["stroke"].replace(np.nan, mean_stroke)
--- IBM_Analysing_data/laptop_data_test.py
+++ IBM_Analysing_data/laptop_data_test.py
@@ -11,15 +11,50 @@ df = df[1:].reset_index(drop=True)
 df = df.drop(df.columns[0], axis=True)
 
 df.tail(50)
+df.head(50)
 df.info()
 
-df[["Screen_Size_cm", "CPU_frequency", "Weight_kg"]] = df[["bore", "stroke"]].astype(
-    "float"
-)
+df[["Screen_Size_cm", "CPU_frequency", "Weight_kg"]] = df[
+    ["Screen_Size_cm", "CPU_frequency", "Weight_kg"]
+].astype("float")
 df[["Category", "Price", "GPU", "OS", "CPU_core", "Storage_GB_SSD", "RAM_GB"]] = df[
-    ["normalized-losses"]
+    ["Category", "Price", "GPU", "OS", "CPU_core", "Storage_GB_SSD", "RAM_GB"]
 ].astype("int")
-df[["price"]] = df[["price"]].astype("float")
-df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
 
-df[["Screen_Size_cm"]] = np.round(df["Screen_Size_cm"], 2)
+
+df["Screen_Size_cm"] = np.round(df["Screen_Size_cm"], 2)
+
+df["Screen"].unique()
+df["Storage_GB_SSD"].unique()
+
+
+missing_data = df.isnull()
+# missing_data.head(50)
+
+
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print(missing_data[column].value_counts())
+    print("")
+
+    # Screen_Size_cm
+    # Screen_Size_cm
+    # False    234
+    # True       4
+    # Name: count, dtype: int64
+
+    # Weight_kg
+    # Weight_kg
+    # False    233
+    # True       5
+    # Name: count, dtype: int64
+
+screen_avg_size = df["Screen_Size_cm"].astype("float").mean(axis=0)
+df["Screen_Size_cm"] = np.round(
+    df["Screen_Size_cm"].replace(np.nan, screen_avg_size), 2
+)
+
+avg_wt = df["Weight_kg"].astype("float").mean(axis=0)
+df["Weight_kg"] = np.round(df["Weight_kg"].replace(np.nan, avg_wt), 2)
+
+df.to_csv("./clean_csv_laptop_pricing_dataset.csv")
